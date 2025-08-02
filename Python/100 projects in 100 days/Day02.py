e6cde2cf9e36d99549a2c04b90dd199eafacae1f
thanhnhadev <thanhnhadev@gmail.com>
2025-08-02 13:42:08 +0700

    day2

---
+++ Python/100 projects in 100 days/Day02.py
@@ -0,0 +1,18 @@
+#Data type, Numbers, Operations, Type Conversion, f-Strings
+# Subscripting
+print(len("Hello"))
+print("hallo"[4])
+print("hallo"[-2])
+street_name = "Abbey Road"
+print(street_name[4] + street_name[7])
+# String
+print("123" + "345") # concatenation
+# Integer = whole number
+print(123 + 345)
+# Large Integers
+print(123_456_789)
+# Float = Floating Point Number
+print(3.14159)
+# Boolean 
+print(True)
+print(False)
