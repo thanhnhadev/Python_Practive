f1f47495a41eb6f94fef0a055d42b6975dfb6e03
Thanhnhadev <thanhnhadev@gmail.com>
2025-08-12 22:20:57 +0700

    BMI Calculator

--- Python/100 projects in 100 days/Day02.py
+++ Python/100 projects in 100 days/Day02.py
@@ -1,4 +1,4 @@
-#Data type, Numbers, Operations, Type Conversion, f-Strings
+# Numbers, Operations, Type Conversion, f-Strings
 # Subscripting
 print(len("Hello"))
 print("hallo"[4])
@@ -16,3 +16,44 @@ print(3.14159)
 # Boolean 
 print(True)
 print(False)
+# Data type
+len("hello")
+print(type("hello"))
+print(type(123))
+print(type(3.14))
+print(type(True))
+int("123")
+print(int("123")+int("456"))
+# print(int("abc")+int("456"))
+int()
+float()
+str()
+bool()
+name_of_the_user = input("enter your name:")
+len_of_name=len(name_of_the_user)
+print(type("number of letter in your name:"))
+print(type(len_of_name))
+print("number of letter in your name: "+ str(len_of_name))
+# Mathematical Operations
+print("My age:"+str(12))
+print(123+456)
+print(7 - 3)
+print(3 * 2)
+print(6 / 3)
+print(6 // 3)
+print(type(6 // 3))
+print(2 ** 3)
+#PEMDASLR
+# ()
+# **
+# *
+# /
+# +
+# -
+print(3*3+3//3-3)
+height = 1.65 
+weight = 84
+# Write your code here.
+# Calculate the bmi using weight and height.
+bmi =weight / height**2
+print(bmi)
