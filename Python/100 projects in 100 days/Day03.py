ddfe25de62b2fe46db94efe32ee298615cebffe0
Thanhnhadev <thanhnhadev@gmail.com>
2025-08-16 15:17:27 +0700

    Day16825

--- Python/100 projects in 100 days/Day03.py
+++ Python/100 projects in 100 days/Day03.py
@@ -1,5 +1,5 @@
-print("welcome to the rollercoaster !")
-height = int(input("what's your height in cm?"))
+# print("welcome to the rollercoaster !")
+# height = int(input("what's your height in cm?"))
 # if height>=120:
 #     print("you can ride the rollercoaster")
 # else:
@@ -8,7 +8,16 @@ height = int(input("what's your height in cm?"))
 #     print("you can ride the rollercoaster")
 # else:
 #     print("sorry you have to grow taller before you can ride.")
-if height!=120:
-    print("you can ride the rollercoaster")
+# if height!=120:
+#     print("you can ride the rollercoaster")
+# else:
+#     print("sorry you have to grow taller before you can ride.")
+
+# even number 12 % 2==0
+number_to_check=int(input("what is the number you want to check?"))
+print(number_to_check)
+print(number_to_check % 2)
+if number_to_check % 2 ==0:
+    print("even")
 else:
-    print("sorry you have to grow taller before you can ride.")
+    print("Odd")
