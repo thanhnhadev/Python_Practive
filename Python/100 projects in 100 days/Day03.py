eca22a00bc6b09dba5fa4a614334498aa0727279
Thanhnhadev <thanhnhadev@gmail.com>
2025-08-17 15:41:18 +0700

    day1708

--- Python/100 projects in 100 days/Day03.py
+++ Python/100 projects in 100 days/Day03.py
@@ -15,9 +15,9 @@
 
 # even number 12 % 2==0
 number_to_check=int(input("what is the number you want to check?"))
-print(number_to_check)
-print(number_to_check % 2)
-if number_to_check % 2 ==0:
-    print("even")
-else:
-    print("Odd")
+# print(number_to_check)
+# print(number_to_check % 2)
+# if number_to_check % 2 ==0:
+#     print("even")
+# else:
+#     print("Odd")
