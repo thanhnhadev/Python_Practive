d5a5dd052fefbb1a2bd50fc94cbf34b539adc973
thanhnha <thanhnhadev@gmail.com>
2025-10-20 13:21:12 +0700

    day04

--- Python/100 projects in 100 days/Day04/task.py
+++ Python/100 projects in 100 days/Day04/task.py
@@ -1,4 +1,4 @@
-import random
+# import random
 # import my_module
 
 # random_integer = random.randint(1, 10)
@@ -12,9 +12,199 @@ import random
 # random_float = random.uniform(1,10)
 # print(random_float)
 # -----------------------------
-random_head_or_tails=random.randint(0,1)
-print(random_head_or_tails)
-if random_head_or_tails ==0:
-    print("mặt ngửa heads")
-else:
-    print("mặt sấp tails")
+# random_head_or_tails=random.randint(0,1)
+# print(random_head_or_tails)
+# if random_head_or_tails == 0:
+#     print("mặt ngửa heads")
+# else:
+#     print("mặt sấp tails")
+# --------------------------------
+# list
+fruits  = ["cherry","apple","pear"]
+# state1 =  "Delaware"
+# state2 = "Pennsylvania"
+# states_of_america =["Delaware","Pennsylvania"]
+# print(states_of_america[0]) # tìm đến vị trí 
+# print(fruits[-1]) # đếm vị trí theo thứ tự 0 trở đi và ngược lại từ dưới đếm lại
+# fruits[1]="organge" #thay vị trí
+# print(fruits)
+# fruits.append("oragagne") # thêm vào cuối
+# print(fruits)
+# fruits.extend(["red","jhon"]) # mở rông thêm 2 dữ liệu trong mảng
+# print(fruits)
+# fruits.insert(3,"htk") # thêm vào vị trí index
+# print(fruits)
+# fruits.remove("htk") # xóa data có chuỗi
+# print(fruits)
+# # fruits.pop([1])
+# # print(fruits)
+# fruits.clear()
+# print(fruits)
+# -------------------------------------------------
+# import random
+# friends =["Alice", "Bob", "Charlie", "David", "Emanuel"]
+# print(random.choice(friends)) # radom 1 data ngẫu nhiên
+# random_index = random.randint(0,4) # cách 2
+# print(friends[random_index])
+# ----------------------------------------------------
+# states_of_america = [
+#     "Delaware",
+#     "Pennsylvania",
+#     "Alabama",
+#     "Alaska",
+#     "Arizona",
+#     "Arkansas",
+#     "California",
+#     "Colorado",
+#     "Connecticut",
+#     "Florida",
+#     "Georgia",
+#     "Hawaii",
+#     "Idaho",
+#     "Illinois",
+#     "Indiana",
+#     "Iowa",
+#     "Kansas",
+#     "Kentucky",
+#     "Louisiana",
+#     "Maine",
+#     "Maryland",
+#     "Massachusetts",
+#     "Michigan",
+#     "Minnesota",
+#     "Mississippi",
+#     "Missouri",
+#     "Montana",
+#     "Nebraska",
+#     "Nevada",
+#     "New Hampshire",
+#     "New Jersey",
+#     "New Mexico",
+#     "New York",
+#     "North Carolina",
+#     "North Dakota",
+#     "Ohio",
+#     "Oklahoma",
+#     "Oregon",
+#     "Rhode Island",
+#     "South Carolina",
+#     "South Dakota",
+#     "Tennessee",
+#     "Texas",
+#     "Utah",
+#     "Vermont",
+#     "Virginia",
+#     "Washington",
+#     "West Virginia",
+#     "Wisconsin",
+#     "Wyoming"
+# ]
+# print(len(states_of_america)) # điếm list danh sách
+# num_of_startes = len(states_of_america) # 50->49
+# print(states_of_america[num_of_startes -1])
+# ----------------------------------------------------
+# fruits = [
+#     "apples",
+#     "bananas",
+#     "oranges",
+#     "grapes",
+#     "strawberries",
+#     "blueberries",
+#     "pears",
+#     "kiwis",
+#     "mangoes",
+#     "pineapples",
+#     "watermelons",
+#     "peaches"
+# ]
+# vegetables = [
+#     "carrots",
+#     "broccoli",
+#     "spinach",
+#     "kale",
+#     "lettuce",
+#     "cucumbers",
+#     "tomatoes",
+#     "peppers",
+#     "onions",
+#     "potatoes",
+#     "garlic",
+#     "zucchini"
+# ]
+# pesticides = [
+#     "glyphosate",
+#     "chlorpyrifos",
+#     "malathion",
+#     "pyrethrins",
+#     "neonicotinoids",
+#     "imidan",
+#     "carbaryl",
+#     "diazinon",
+#     "rotenone",
+#     "captan",
+#     "parathion",
+#     "dicamba"
+# ]
+# dirty_dozen =[vegetables, fruits] # gộp 2 list danh sách
+# print(dirty_dozen)
+# ----------------------------------------------------
+# fruits = ["Strawberries", "Nectarines", "Apples", "Grapes", "Peaches", "Cherries", "Pears"]
+# print(fruits[-5])
+# ----------------------------------------------------
+# fruits = ["Strawberries", "Nectarines", "Apples", "Grapes", "Peaches", "Cherries", "Pears"]
+# fruits[-1] = "Melons"
+# fruits.append("Lemons")
+# print(fruits)
+# ----------------------------------------------------
+# fruits = ["Strawberries", "Nectarines", "Apples", "Grapes", "Peaches", "Cherries", "Pears"]
+# vegetables = ["Spinach", "Kale", "Tomatoes", "Celery", "Potatoes"]
+# dirty_dozen = [fruits, vegetables]
+# print(dirty_dozen[1][1])
+# # print(dirty_dozen)
+# # print(dirty_dozen[0])
+# # print(dirty_dozen[1])
+# # print(dirty_dozen[1][2])
+# # print(dirty_dozen[1][3])
+# ----------------------------------------------------
+import random
+rock = '''
+   _______
+  /       \ 
+ /         |
+|    ( )   |
+|    ( )   |
+|    ( )   |
+ \         |
+  \_______/
+'''
+
+paper = '''
+   _______
+  /       \ 
+ /         |
+|    _____ |
+|   |     || 
+|   |     ||
+ \_______/
+'''
+
+scissors = '''
+   _______
+  /       \ 
+ /         |
+|   / \    |
+|  /   \   |
+| /     \  |
+ \         |
+  \_______/
+'''
+# In các lựa chọn
+# print("Búa:")
+# print(rock)
+# print("Bao:")
+# print(paper)
+# print("Kéo:")
+# print(scissors)
+user_choice = input("what do you choose? type 0 for rock, 1 for Paper of 2 for Scissors \n")
+computer_choice = random.randint(0, 2)
+print(f"Computer chose {computer_choice}")
