652f87f5f0aa85043a0db8df6ada20c9575ce3a2
thanhnha <thanhnhadev@gmail.com>
2025-11-09 14:31:05 +0700

    ai

--- Fast_Api/routers/multiple_parmeters.py
+++ Fast_Api/routers/multiple_parmeters.py
@@ -1,7 +1,62 @@
-
+# Body - Multiple Parameters
 from fastapi import APIRouter, Query, Path
+from pydantic import BaseModel
 router = APIRouter()
 
-@router.get("/user")
-async def get_user():
-    return {"message": "Hello-World"}
+class Item(BaseModel):
+    name: str
+    decriptipn: str | None = None
+    price: float
+    tax: float| None = None
+
+@router.post("/items")
+async def create_item(item: Item):
+    item_dict = item.dict()
+    if item.tax:
+        price_with_tax = item.price + item.tax
+        item_dict.update({"price_with_tax": price_with_tax})
+    return item_dict
+
+@router.put("/items/{item_id}")
+async def create_item_with_put(item_id: int, item: Item, q: str | None = None):
+    result = {"item_id": item_id, **item.dict()}
+    if q:
+        result.update({"q": q})
+    return result
+
+@router.get("/items")
+async def read_items(
+    q: str
+    | None = Query(
+        None,
+        min_length=3,
+        max_length=10,
+        title="Sample query string",
+        description="This is a sample query string.",
+        alias="item-query",
+    )
+):
+    results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
+    if q:
+        results.update({"q": q})
+    return results
+
+@router.get("/items_hidden")
+async def hidden_query_route(
+    hidden_query: str | None = Query(None, include_in_schema=False)
+):
+    if hidden_query:
+        return {"hidden_query": hidden_query}
+    return {"hidden_query": "Not found"}
+
+@router.get("/items_validation/{item_id}")
+async def read_items_validation(
+    *,
+    item_id: int = Path(..., title="The ID of the item to get", gt=10, le=100),
+    q: str = "hello",
+    size: float = Query(..., gt=0, lt=7.75)
+):
+    results = {"item_id": item_id, "size": size}
+    if q:
+        results.update({"q": q})
+    return results
--- README.md
+++ README.md
@@ -6,5 +6,5 @@
 # trong thu muc Fast_API
 # tao file .env
 coppy api key vao
-
-# OPENAI_API_KEY=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
+create open apikey:https://platform.openai.com/api-keys
+OPENAI_API_KEY= api key
